0d4145aed4b12fd958ea8e44c5a4ee32887dc851
Bernardo Werneck <bernardo.s.werneck@gmail.com>
2019-10-31 11:00:27 -0300

    Aplicação do hibernate

---
+++ src/main/java/com/gplenty/quiz/AbstractDao.java
@@ -0,0 +1,40 @@
+package com.gplenty.quiz;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+
+public abstract class AbstractDao<Type> 
+{
+
+	private EntityManagerFactory emf;
+	private EntityManager em;
+	private Class<Type> typeClass;
+	
+	public AbstractDao(Class<Type> typeClass)
+	{
+		Type t;
+		emf = Persistence.createEntityManagerFactory("MariaDBPersistence");
+		em = emf.createEntityManager();
+		this.typeClass = typeClass;
+	}
+	
+	public List<Type> retrieveAll()
+	{
+		String queryString = "Select t from " + typeClass.getSimpleName() + " t";
+		Query query = em.createQuery(queryString);
+		return query.getResultList();
+	}
+	
+	public void save(Type element)
+	{
+		em.persist(element);
+		em.getTransaction().begin();
+		em.getTransaction().commit();
+	}
+	
+}
